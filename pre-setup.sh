8df83c68e5ed363e4d67c98fc8c9a97c849681bc
BoyiChen <21008254+boyic-git@users.noreply.github.com>
2021-03-22 22:44:29 +0800

    add pre-setup

--- pre-setup.sh
+++ pre-setup.sh
@@ -6,6 +6,10 @@ sudo apt-get install build-essential libssl-dev libpq-dev libcurl4-gnutls-dev li
 
 sudo apt-get install supervisor -y 
 
+sudo apt-get install python3-pip python3-dev python3-venv -y
+
+sudo apt-get install nano -y
+
 sudo apt-get install git -y 
 
 sudo apt-get install nginx curl -y
@@ -16,6 +20,16 @@ sudo ufw allow 'Nginx Full'
 
 sudo ufw allow ssh
 
+sudo python3 -m pip install jupyter
+
 sudo service supervisor start
 
 sudo apt autoremove -y
+
+git clone https://github.com/boyic-git/aws-notebook-environment-setup.git
+
+chmod +x ./aws-notebook-environment-setup/setup.sh
+
+sudo ./aws-notebook-environment-setup/setup.sh
+
+sudo apt-get install ec2-instance-connect
