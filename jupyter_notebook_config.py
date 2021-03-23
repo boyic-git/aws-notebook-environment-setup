aa10539c0edc74898fe44bc06b2f0d7bfbed29ea
BoyiChen <21008254+boyic-git@users.noreply.github.com>
2021-03-23 17:20:49 +0800

    change notebook password to default "admin"

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -9,5 +9,5 @@ c.NotebookApp.allow_origin = '*' # put your public IP Address here
 c.NotebookApp.ip = '*'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'sha1:6c21e610cb9f:2fc4c4bd87d4f522c704e68111b7f1a7e7b34dce'
+c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$1188xGgDIDs49qtL/Hh/Cg$su7QTqBgJSHfkJ5XpTmmiw'
 c.NotebookApp.port = 8888
