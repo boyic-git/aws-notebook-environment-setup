418fb7b690ecbd71c89e6d522a9adfab8ee4afc8
BoyiChen <21008254+boyic-git@users.noreply.github.com>
2021-04-02 12:49:37 +0800

    add kaggle and wanbd

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+pip3 install kaggle==1.5.6 --user
+
+pip3 install wandb --user
+
+sudo ln -s ~/.local/bin/kaggle /usr/bin/kaggle
+
+sudo ln -s ~/.local/bin/wandb /usr/bin/wandb
+
 jupyter notebook --generate-config
 
 sudo cp aws-notebook-environment-setup/jupyter_notebook_config.py  /home/ubuntu/.jupyter/
